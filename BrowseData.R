1f970d85fe22b78b1953b1164e1b2f12a0d5ab5c
jaywarrick <jay.w.warrick@gmail.com>
2016-02-03 01:26:09 -0600

    Update UI

--- BrowseData.R
+++ BrowseData.R
@@ -1,12 +1,13 @@
 rm(list=ls())
 source('~/.Rprofile')
-library(foreign)
-library(shiny)
-library(plotly)
 library(data.table)
+
+library(foreign)
 fileTable <- read.arff('/Users/jaywarrick/Documents/JEX/Feature Extraction/temp/JEXData0000000055.arff')
 
 temp <- read.arff(fileTable$Value[1])
 setorder(temp, Id, Label, MaskChannel_ImageChannel, Measurement)
 shinyData <- data.table(reorganize(temp, idCols = c('Id'), measurementCols=c('Measurement','MaskChannel_ImageChannel'), valueCols=c('Value')))
-runApp('/Users/jaywarrick/Public/DropBox/GitHub/R-General/DataBrowser')
+sourceGitHubFile(user='jaywarrick', repo='R-General', branch='master', file='DataBrowser/ui.R')
+sourceGitHubFile(user='jaywarrick', repo='R-General', branch='master', file='DataBrowser/server.R')
+shinyApp(ui=myUI, server=myServer)
--- DataBrowser/ui.R
+++ DataBrowser/ui.R
@@ -8,15 +8,15 @@ myUI <- shinyUI(fluidPage(
      selectInput("Label", label = h3("Label"),
                  choices = names(shinyData),
                  selected = 1,
-                 width='100%'),
+                 width='50%'),
      selectInput("X", label = h3("X-Axis"),
                  choices = names(shinyData)[sapply(shinyData, is.numeric)],
                  selected = 1,
-                 width='100%'),
+                 width='50%'),
      selectInput("Y", label = h3("Y-Axis"),
                  choices = names(shinyData)[sapply(shinyData, is.numeric)],
                  selected = 1,
-                 width='100%'),
+                 width='50%'),
      mainPanel(
           plotlyOutput("plot"), height='5000px'
      ),
